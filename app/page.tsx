0bce93c529664f83c5c6c00670af92d23c8262dc
Daniel Yadghar <daniel072.dy@gmail.com>
2025-11-26 15:10:40 +0330

    Update path of first memeto pattern example

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import FormWithUndo from "./design_patterns/memento/FormWithUndo";
+import FormWithUndo from "./design_patterns/memento/example_one/FormWithUndo";
 
 const Home = () => {
   return (
